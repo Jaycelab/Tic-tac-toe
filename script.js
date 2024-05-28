8547d59c5e244fff868906a43fa599c10c5e27f6
Jason Alvarado <jayxalvarado@gmail.com>
2024-05-27 18:29:16 -0700

    Finished delay math function and Bot function

--- script.js
+++ script.js
@@ -68,16 +68,71 @@ let playerSignValue = "X"; //setting the player sign value to X
 let runBot = true; //setting the runBot to true
 
 //click box function for handling the click on users keyboard
-function clickedBox(e) {
+function clickedBox(element) {
   if (playerTurn.classList.contains("Player")) {
     playerSign = "O"; //if the player turn contains the player class, set the player sign to O
     playerSignValue = "O"; //if the player turn contains the player class, set the player sign value to O
     element.innerHTML = `<i class = "${playerOIcon}"></i>`; //set the innerHTML of the element to the player O icon
-    playerTurn.classList.remove("active");
-    element.setAttribute("id", playerSign);
+    playerTurn.classList.remove("active"); //remove the active class from the player turn element
+    element.setAttribute("id", playerSign); //set the id of the element to the player sign
   } else {
-    element.innerHTML = `<i class = "${playerXIcon}"></i>`; //set the innerHTML of the element to the player X icon
+    element.innerHTML = `<i class = "${playerXIcon}"></i>`; //if the player turn does not contain the player class, set the innerHTML of the element to the player X icon and id to the player sign using setAttribute
     element.setAttribute("id", playerSign);
-    playerTurn.classList.add("active");
+    playerTurn.classList.add("active"); //add the active class to the player turn element
   }
+
+  //overall after each move, select winner function if there is a winner
+  selectWinner(); //calling the selectWinner function
+
+  element.style.pointerEvents = "none"; //disables click events for the click box
+  setContainer.style.pointerEvents = "none"; //siables click event for entire game point
+
+  //***timeout function to delay the bot move***
+  //temp disable click events on section  container to prevent multiple clicks
+  let randomTimeDelay = (Math.random() * 1000 + 200).toFixed(); //setting the random time delay to a random number between 200 and 1000
+  setTimeout(() => {
+    bot(runBot); //calling the bot function
+  }, randomTimeDelay); //setting the random time delay
+}
+
+// BOT FUNCTION
+
+function bot() {
+  let array = [];
+  if (runBot) {
+    playerSign = "O"; //setting the player sign to O
+  }
+  //iterating through the secSpan array to check there are any available tiles or sets
+  for (let i = 0; i < secSpan.length; i++) {
+    //if child element count of the secSpan is 0, push the index of the element to the array
+    if (secSpan[i].childElementCount === 0) {
+      array.push(i);
+    }
+  }
+
+  /*
+  1. Randomly select an index from the array of available tiles using math.random and math.floor
+  2. Check if there are any avaialble tiles in the array to select. If there are, check if the player turn contains the player class and set the player sign to along with the player X icon
+  3. If there are no available tiles, set the player sign to O and the player O icon
+  4. Once the conditions have been checked, it runs the selectWinner function and determines if there is a winner or if it is a draw
+  5. Disables the click events on the select tile to prevent interuption and enables the click events on the section container for the next game turn
+  */
+  let randomBox = array[Math.floor(Math.random() * array.length)];
+  if (array.length > 0) {
+    if (playerTurn.classList.contains("player")) {
+      playerSign = "X";
+      secSpan[randomBox].innerHTML = `<i class = "${playerXIcon}"></i>`;
+      secSpan[randomBox].setAttribute("id", playerSign);
+      playerTurn.classList.add("active");
+    }
+  } else {
+    secSpan[randomBox].innerHTML = `<i class = "${playerOIcon}"></i>`;
+    playerTurn.classList.remove("active");
+    secSpan[randomBox].setAttribute("id", playerSign);
+  }
+  selectWinner();
+
+  secSpan[randomBox].style.pointerEvents = "none";
+  secContainer.style.pointerEvents = "auto";
+  playerSign = "X";
 }
